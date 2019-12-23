0e99837d2df6ff40a5c418a71a917b83de80b3d5
lenaonisch <elenaonishhenk0@gmail.com>
2019-12-23 22:49:25 +0200

    UI added

---
+++ 5_NumberToWords/NumberConverterApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _5_NumberToWords.Numbers;
+
+namespace NumberConverterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberConverter converter = new NumberConverter();
+            int numberToConvert;
+            
+            do
+            {
+                if (UI.GetNumberFromConsole(out numberToConvert))
+                {
+                    Console.WriteLine(NumberConverter.GetStringRepresentation(numberToConvert));
+                }
+            }
+            while (UI.IsContinued());
+        }
+    }
+}
---
+++ 5_NumberToWords/NumberConverterApplication/UI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConverterApplication
+{
+    class UI
+    {
+        public static bool GetNumberFromConsole(out int number)
+        {
+            Console.WriteLine("Please enter number from -2 147 483 646 to 2 147 483 647:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid number...");
+                return false;
+            }
+        }
+
+        public static bool IsContinued()
+        {
+            Console.WriteLine("Do you want to continue (y/yes)?");
+            string userAnswer = Console.ReadLine().ToLower();
+            return (userAnswer == "y" || userAnswer == "yes");
+        }
+    }
+}
